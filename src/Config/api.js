49816a6eea78f478f797122f4ee828ef7336e244
Nitin Kumar <nitin.kumar@deutsche-telekom.com>
2021-10-27 19:48:13 +0100

    cors anywhere added

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,7 +1,7 @@
 import { axios } from "./interceptors";
-// const BASE_URL =
-//   "https://cors-anywhere.herokuapp.com/https://api.smarkets.com/v3/";
-const BASE_URL = "https://api.smarkets.com/v3/";
+const BASE_URL =
+  "https://cors-anywhere.herokuapp.com/https://api.smarkets.com/v3/";
+// const BASE_URL = "https://api.smarkets.com/v3/";
 
 export const getPopularEvents = () => {
   return axios.get(BASE_URL + "popular/event_ids/");
